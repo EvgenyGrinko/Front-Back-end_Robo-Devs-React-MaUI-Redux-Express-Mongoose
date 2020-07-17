d5fbae302e2113c710705154cecc97eb1b3b37e3
EvenyGrinko <john_forever_young@mail.ru>
2020-07-17 12:58:18 +0300

    Add handlers for POST and DELETE requests

--- api/app.js
+++ api/app.js
@@ -1,9 +1,9 @@
 const express = require("express");
-const mongoose = require("mongoose");
-const bodyParser = require("body-parser");
 const dotenv = require("dotenv");
 const path = require("path");
-const connectDB = require('./config/db');
+const connectDB = require("./config/db");
+const bodyParser = require("body-parser");
+
 
 dotenv.config({ path: "./config/config.env" });
 
@@ -13,13 +13,19 @@ const transactions = require("./routes/transactions");
 
 const app = express();
 
+app.use(bodyParser.json());
+app.use(express.json());
+
+
 app.use("/api/v1/transactions", transactions);
 
 app.use(
   express.static(path.join("/home/user/Desktop/robo-developers/client/"))
 );
 
+
 const PORT = process.env.PORT || 4000;
+
 app.listen(PORT, () => {
   console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`);
 });
--- api/models/Transaction.js
+++ api/models/Transaction.js
@@ -3,11 +3,11 @@ const mongoose = require("mongoose");
 const TransactionSchema = new mongoose.Schema({
   text: {
     type: String,
-    require: [true, "Please add some text"],
+    required: [true, "Please add some text"],
   },
   amount: {
     type: Number,
-    require: [true, "Please add a number"],
+    required: [true, "Please add a number"],
   },
   createdAt: {
     type: Date,
